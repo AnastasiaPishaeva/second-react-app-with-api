da1fdc16214c6907b8dfd6741b47d4f1b1b86748
AnastasiaPishaeva <n_pishaeva@mail.ru>
2025-12-04 23:14:40 +0300

    feat: add content on meowPage

--- src/pages/meowFacts.tsx
+++ src/pages/meowFacts.tsx
@@ -12,6 +12,7 @@ const Title = styled(Typography)`
   font-size: 58px;
   font-weight: 800;
   line-height: normal;
+  text-align: center; 
   letter-spacing: 0.2px;
   color: #121212;
   font-family: 'Montserrat', sans-serif;
@@ -62,9 +63,17 @@ const NumberField = styled(TextField)`
     }
 `;
 
+const ErrorMessage = styled(Typography)`
+    font-size: 12px;
+    color: #e47359;
+    font-weight: bold;
+    margin-top: 5px;
+    margin-bottom: 12px;
+`
+
 const CustomButton = styled(Button)`
-  padding: 10px 20px;
-  margin-top: 16px;
+  padding: 3px 20px;
+  margin-top: 4px;
   font-size: 16px;
   font-weight: bold;
   background: linear-gradient(to bottom, #ececdf, #CDC6B3);
@@ -111,28 +120,33 @@ const CustomButton = styled(Button)`
     }
 `;
 
-
-
-
+const StyledCard = styled(Card)`
+    background-color: #F6F6F3;
+    padding: 20px 40px;
+    border-radius: 5px;
+    box-shadow: 0 15px 35px rgba(0, 0, 0, 0.15);
+`
 const MeowPage = () => {
     const[data, setData] = useState<string[]>([]);
     const[count, setCount] = useState(""); 
     const [message, setMessage] = useState("");
+    const [flag, setFlag] = useState(false);
 
-    const fetchFacts = async () => {
+    const fetchFacts = async (params: string) => {
         try {
-        const res = await api.get(`/`, {params: count});
+        const res = await api.get(`/`, {params: {count}});
         console.log("данные с сервера:", res.data);
-        setData(res.data);
+        setData(res.data.data);
         } catch (err) {
         console.error("Ошибка загрузки:", err);
         }
     }; 
 
-    function validateAndSet(value: string) {
+    function validate(value: string) {
         if (value.trim() === "") {
             setCount("");
             setMessage("Введите число!");
+            setFlag(false);
             return;
         }
 
@@ -140,17 +154,20 @@ const MeowPage = () => {
 
         if (!Number.isInteger(number) || number <= 0) {
             setMessage("Вводите только целое положительное число!");
+            setFlag(false);
             return;
         }
 
         setMessage("");
         setCount(value);
+        setFlag(true);
         }
     
-    
-    useEffect(() => {
-        fetchFacts();
-    }, [count]);
+    function request() {
+      if (flag === true){
+        fetchFacts(count);
+      }
+    }
 
     return(
         <Grid container direction={'column'} alignItems="center">
@@ -160,10 +177,34 @@ const MeowPage = () => {
                 placeholder="Enter tne number of facts"
                 value={count}
                 onChange={(e) => {
-                    validateAndSet(e.target.value);
+                    validate(e.target.value);
                 }}
             />
-            <CustomButton>Generate</CustomButton>
+            <ErrorMessage>{message}</ErrorMessage>
+            <CustomButton onClick={request}>
+              Generate</CustomButton>
+            <Grid container spacing={3} sx= {{
+              width: "100%",
+              marginTop: "30px",
+              justifyContent: "center"
+            }}>
+              {data.map((fact: string, index: number) => (
+                  <Grid size={{ xs: 10 }} 
+                  key={index}
+                  sx={{       
+                      transition: "0.3s",
+                      "&:hover": {
+                          transform: "scale(1.02)",
+                      }
+                  }}
+                  >
+                  <StyledCard>
+                    <Typography sx = {{fontFamily : 'Montserrat', fontSize : "16px", fontWeight : "500"}}>Fact №{index+1}</Typography>
+                    <Typography sx = {{fontFamily : 'Montserrat', fontSize : "16px", fontWeight : "500"}}>{fact}</Typography>
+                  </StyledCard>
+                  </Grid>
+              ))}
+          </Grid>
         </Grid>
     )
 } 
